abf1afdb4630551ceac662e3422ea2ce298a4b2f
parker cheng <fbi0258zzz@gmail.com>
2023-01-12 18:51:08 +0800

    connect to mongodb altas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ec695d29943fb07"; }
+/******/ 		__webpack_require__.h = function() { return "40aa9028d9d15f1c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
