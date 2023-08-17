745c0e5e773be0f87fdc6b82ba29303c1faa6590
parker cheng <fbi0258zzz@gmail.com>
2023-08-17 18:39:35 +0800

    prisma studio

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,11 +6,6 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from './db/client';
 import { env } from '@/env/server.mjs';
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
     callbacks: {
         session: ({ session, user }) => ({
@@ -23,31 +18,13 @@ export const authOptions: NextAuthOptions = {
     },
     adapter: PrismaAdapter(prisma),
     providers: [
-        // DiscordProvider({
-        //   clientId: env.DISCORD_CLIENT_ID,
-        //   clientSecret: env.DISCORD_CLIENT_SECRET,
-        // }),
         GoogleProvider({
             clientId: env.GOOGLE_CLIENT_ID,
             clientSecret: env.GOOGLE_CLIENT_SECRET,
         }),
-        /**
-         * ...add more providers here.
-         *
-         * Most other providers require a bit more work than the Discord provider. For example, the
-         * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-         * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-         *
-         * @see https://next-auth.js.org/providers/github
-         */
     ],
 };
 
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
 export const getServerAuthSession = (ctx: {
     req: GetServerSidePropsContext['req'];
     res: GetServerSidePropsContext['res'];
